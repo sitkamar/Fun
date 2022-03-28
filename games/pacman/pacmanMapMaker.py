ba385f4a35b6417915f33d623dda59e87d261829
sitkamar <martin.sitina@student.gyarab.cz>
2022-03-28 08:52:44 +0200

    fixedTicTacToe

--- games/pacman/pacmanMapMaker.py
+++ games/pacman/pacmanMapMaker.py
@@ -7,10 +7,11 @@ ghosts = []
 pacman = vector(0, 0)
 path = Turtle(visible=False)
 jsonFile = "D:\Programming\Fun\games\pacman\levels.json"
-reader = open(jsonFile,'r')
+reader = open(jsonFile, 'r')
 levels = json.load(reader)
 reader.close()
 state = {'mode': 'n', 'level': len(levels["levels"])}
+
 
 def initialize():
     global tiles, pacman, ghosts, levels
@@ -21,12 +22,15 @@ def initialize():
         ghostsHelp = levels["levels"][state['level']]["ghosts"]
         for ghost in ghostsHelp:
             ghosts.append(vector(ghost[0], ghost[1]))
-        pacman = vector(levels["levels"][state['level']]["pacman"][0], levels["levels"][state['level']]["pacman"][1])
+        pacman = vector(levels["levels"][state['level']]["pacman"]
+                        [0], levels["levels"][state['level']]["pacman"][1])
     else:
         tiles = [0] * 400
         ghosts = []
         pacman = vector(0, 0)
     world()
+
+
 def square(x, y):
     path.up()
     path.goto(x, y)
@@ -36,6 +40,7 @@ def square(x, y):
         path.forward(20)
         path.left(90)
     path.end_fill()
+
 
 def world():
     bgcolor('black')
@@ -68,8 +73,9 @@ def world():
     dot(20, 'yellow')
     update()
 
+
 def click(x, y):
-    newX = (floor(x,20) + 200) / 20
+    newX = (floor(x, 20) + 200) / 20
     newY = (180 - floor(y, 20)) / 20
     index = int(newX + newY * 20)
     if tiles[index] == 0:
@@ -82,39 +88,44 @@ def click(x, y):
         tiles[index] = 0
     world()
     update()
+
+
 def send():
     global jsonFile
-    writer = open(jsonFile,'w')
+    writer = open(jsonFile, 'w')
     ghostsHelp = []
     save = input('Save as new(n), replace(r): ')
     for ghost in ghosts:
         ghostsHelp.append([ghost.x, ghost.y])
-    newLevel = {"tiles": tiles, "pacman": [pacman.x, pacman.y], "ghosts": ghostsHelp}
+    newLevel = {"tiles": tiles, "pacman": [
+        pacman.x, pacman.y], "ghosts": ghostsHelp}
     if save == 'n':
         num = len(levels["levels"])+1
         levels["levels"][num] = newLevel
     elif save == 'r':
         levels["levels"][state['level']] = newLevel
     json.dump(levels, writer)
-def figurs(x,y):
+
+
+def figurs(x, y):
     global pacman
-    if vector(floor(x,20),floor(y,20)) in ghosts:
-        pacman = vector(floor(x,20), floor(y,20))
-        ghosts.remove(vector(floor(x,20),floor(y,20)))
-    elif vector(floor(x,20), floor(y,20)) == pacman:
+    if vector(floor(x, 20), floor(y, 20)) in ghosts:
+        pacman = vector(floor(x, 20), floor(y, 20))
+        ghosts.remove(vector(floor(x, 20), floor(y, 20)))
+    elif vector(floor(x, 20), floor(y, 20)) == pacman:
         pacman = vector(0, 0)
     else:
-        ghosts.append(vector(floor(x,20), floor(y,20)))
+        ghosts.append(vector(floor(x, 20), floor(y, 20)))
     world()
+
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 initialize()
 listen()
-onscreenclick(lambda x, y: click(x,y),1)
-onscreenclick(lambda x, y: figurs(x,y),3)
+onscreenclick(lambda x, y: click(x, y), 1)
+onscreenclick(lambda x, y: figurs(x, y), 3)
 onkey(send, 'space')
 world()
 done()
-
