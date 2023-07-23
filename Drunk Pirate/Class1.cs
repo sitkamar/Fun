284a268a1e255aa1fb12467c1a95c68d2bf46d86
sitkamar <71065759+sitkamar@users.noreply.github.com>
2023-07-23 09:34:51 +0200

    Drunk Pirate

---
+++ Drunk Pirate/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+public class Player
+{
+	public string name;
+}
+public class Card
+{
+	public Bitmap picture;
+	public string dare;
+}
+public class Game
+{
+	private string cardFile;
+	private Player[] players;
+
+}
---
+++ Drunk Pirate/Drunk Pirate/Form1.cs
@@ -0,0 +1,10 @@
+namespace Drunk_Pirate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
